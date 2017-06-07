621fa3cde315e5fb8338e23fd5f87bce7d8f053f
Sarah Marsh <Sarah.Marsh@arm.com>
2017-06-07 12:05:34 -0500

    Test writers modify only mbed_app.json

--- net-help.h
+++ net-help.h
@@ -1,14 +1,11 @@
-#define ETHERNET        1
-#define WIFI_ESP8266    2
-#define MESH_LOWPAN_ND  3
-#define MESH_THREAD     4
-#define WIFI_ODIN       5
-#define CELL_C027       6
+#include "mbed.h"
+#include MBED_CONF_APP_HEADER_FILE
 
-#if MBED_CONF_APP_NETWORK_INTERFACE == ETHERNET
-#include "net-help-eth.h"
-#endif 
+NetworkInterface* get_net() {
+    NetworkInterface* net = MBED_CONF_APP_OBJECT_CONSTRUCTION;
+    return net;
+}
 
-#if MBED_CONF_APP_NETWORK_INTERFACE == CELL_C027
-#include "net-help-c027.h"
-#endif 
+int net_connect(NetworkInterface* net) {
+    return MBED_CONF_APP_CONNECT_STATEMENT;
+}
